2e87ad289906dc44013d2a88eaa068e62d749a7b
sodino <sodino@qq.com>
2021-08-08 12:50:05 +0800

    First Commit

---
+++ Launch.py
@@ -0,0 +1,48 @@
+from xml.dom.minidom import parse
+import xml.dom.minidom
+import os
+
+## 寻找老素材的地址
+dir_old = "/Users/sodino/NativeProjects/textconfiguration/old"
+## 记录所有的Tag名称
+tags = {}
+
+class TextXML:
+    count = 0
+    def read2parse_xml(self, path):
+        dom_tree = xml.dom.minidom.parse(path)
+        root_element = dom_tree.documentElement
+        for child in root_element.childNodes:
+            if child.nodeType == xml.dom.Node.TEXT_NODE:
+                continue
+
+            print("child.nodeName=" + child.nodeName  + " nodeType=" + str(child.nodeType) + " childNodes.length=" + str(child.childNodes.length))
+            ## 为了使用dict记录 nodeName，value无意义
+            tags[child.nodeName] = ""
+            if (child.childNodes.length > 0):
+                for c2 in child.childNodes:
+                    if (c2.nodeType == xml.dom.Node.TEXT_NODE) :
+                        continue
+                    tags[c2.nodeName] = ""
+
+
+
+
+def findAllFile(base):
+    for root, ds, fs in os.walk(base):
+        for f in fs:
+            if f.endswith("TextBubbleInfo.xml"):
+                fullname = os.path.join(root, f)
+                yield fullname
+
+
+if __name__ == '__main__':
+    ##  当前目录路径
+    # current_path = os.getcwd()
+    # test_xml = TextXML()
+    # test_xml.read2parse_xml(current_path + "/TextBubbleInfo.xml")
+    for i in findAllFile(dir_old) :
+        test_xml = TextXML()
+        test_xml.read2parse_xml(i)
+
+    print("xml all tagNames are : " + str(tags.keys()))
