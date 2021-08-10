7c81cff302094734e26cfa942aefdfe3582a7640
sodino <sodino@qq.com>
2021-08-10 11:26:37 +0800

    [dev] 添加 XmlConst.py 用于定义字符串常量

---
+++ XmlConst.py
@@ -0,0 +1,16 @@
+const_root_plist = '''
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist>
+	<array>
+		<dict>
+			<key>AR</key>
+			<string>ar</string>
+			<key>BubbleSize</key>
+			<string>{width},{height}</string>
+			<key>TextFontKey</key>
+			<string>{fonts}</string>
+		</dict>
+	</array>
+</plist>
+'''
